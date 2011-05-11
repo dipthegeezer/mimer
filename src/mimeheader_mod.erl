91de0adf20e379ac0a2b36063d1f15d8f75a72fd
Dipesh Patel <dipthegeezer.opensource@gmail.com>
2011-05-11 23:20:29 +0100

    More file parsing

--- src/mimeheader_mod.erl
+++ src/mimeheader_mod.erl
@@ -10,67 +10,14 @@
 %% @doc Given a filename extension (e.g. ".html") return a guess for the MIME
 %%      type such as "text/html". Will return the atom undefined if no good
 %%      guess is available.
-type_from_file_extension(".html") ->
-    "text/html";
-type_from_file_extension(".xhtml") ->
-    "application/xhtml+xml";
-type_from_file_extension(".xml") ->
-    "application/xml";
-type_from_file_extension(".css") ->
-    "text/css";
-type_from_file_extension(".js") ->
-    "application/x-javascript";
-type_from_file_extension(".jpg") ->
-    "image/jpeg";
-type_from_file_extension(".gif") ->
-    "image/gif";
-type_from_file_extension(".png") ->
-    "image/png";
-type_from_file_extension(".swf") ->
-    "application/x-shockwave-flash";
-type_from_file_extension(".zip") ->
-    "application/zip";
-type_from_file_extension(".bz2") ->
-    "application/x-bzip2";
-type_from_file_extension(".gz") ->
-    "application/x-gzip";
-type_from_file_extension(".tar") ->
-    "application/x-tar";
-type_from_file_extension(".tgz") ->
-    "application/x-gzip";
-type_from_file_extension(".txt") ->
-    "text/plain";
-type_from_file_extension(".doc") ->
-    "application/msword";
-type_from_file_extension(".pdf") ->
-    "application/pdf";
-type_from_file_extension(".xls") ->
-    "application/vnd.ms-excel";
-type_from_file_extension(".rtf") ->
-    "application/rtf";
-type_from_file_extension(".mov") ->
-    "video/quicktime";
-type_from_file_extension(".mp3") ->
-    "audio/mpeg";
-type_from_file_extension(".z") ->
-    "application/x-compress";
-type_from_file_extension(".wav") ->
-    "audio/x-wav";
-type_from_file_extension(".ico") ->
-    "image/x-icon";
-type_from_file_extension(".bmp") ->
-    "image/bmp";
-type_from_file_extension(".m4a") ->
-    "audio/mpeg";
-type_from_file_extension(".m3u") ->
-    "audio/x-mpegurl";
-type_from_file_extension(".exe") ->
-    "application/octet-stream";
-type_from_file_extension(".csv") ->
-    "text/csv";
-type_from_file_extension(_) ->
-    undefined.
 
+type_from_file_extension( MimeFileExtList, Filename ) ->
+    case lists:keyfind(filename:extension(Filename), 1, MimeFileExtList) of
+        false ->
+            undefined;
+        {_, MimeType} ->
+            MimeType
+    end.
 
 parse_mime_glob(File) ->
     Data =
@@ -85,7 +32,20 @@ parse_mime_glob(File) ->
             io:fwrite(standard_error,"File error ~p ~p", [Reason, File])
     end,
     Lines = re:split(Data, "\r\n|\n|\r|\032", [{return, list}]),
-    Lines.
+    ParsedValues =
+    lists:foldl(Fun(Line, FileInfo) ->
+                       case string:strip(Line) of
+                           "#" ++ _Comment ->
+                               FileInfo;
+                           Line2 ->
+                               case re:split(Line2, "\s?:\s?", [{return, list}]) of
+                                   [MimeType,Ext] ->
+                                       [{Ext,Mime}|FileInfo];
+                                   _ -> FileInfo
+                               end
+                       end
+               end,[{"",undefined}],Lines),
+    {ok,ParsedValues}.
 
 %%
 %% Tests
@@ -94,16 +54,17 @@ parse_mime_glob(File) ->
 -ifdef(TEST).
 
 type_from_file_extension_test() ->
+    FileInfo = [{".html","text/html"}],
     ?assertEqual("text/html",
-                 type_from_file_extension(".html")),
+                 type_from_file_extension(FileInfo, "monkey.html")),
     ?assertEqual(undefined,
-                 type_from_file_extension("")),
+                 type_from_file_extension(FileInfo,"")),
     ?assertEqual(undefined,
-                 type_from_file_extension(".voodoo_people")),
+                 type_from_file_extension(FileInfo,"magic_people.voodoo_people")),
     ok.
 
 file_load_test() ->
-    Data = parse_mime_glob("/usr/share/mime/globs"),
+    {ok,Data} = parse_mime_glob("/usr/share/mime/globs"),
     io:fwrite(standard_error,"THE DATA ~p", [Data]),
     ?assert(true),
     ok.
